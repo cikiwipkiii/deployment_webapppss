feaaa83c62b6a53d9c8a34e8962a06f96a3f8de4
cikiwipkiii <131977515+cikiwipkiii@users.noreply.github.com>
2023-05-16 20:28:12 +0700

    mainnn.py

--- mainnn.py
+++ mainnn.py
@@ -14,16 +14,8 @@ with st.sidebar :
  #Pengertian Mol
 if Pilihan == "Home" :
  
-    st.header ('HELLO EVERYBODY, WELCOME TO :blue[OUR WEB]! :sunglasses:')
-    file_ = open("https://github.com/cikiwipkiii/deployment_webapppss/blob/main/gambar_1.gif", "rb")
-    contents = file_.read()
-    data_url = base64.b64encode(contents).decode("utf-8")
-    file_.close()
-
-    st.markdown(
-        f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-        unsafe_allow_html=True,
-                )
+   """### gif from url"""
+    st.markdown("![Alt Text](https://github.com/cikiwipkiii/deployment_webapppss/blob/main/gambar_1.gif)")
     
     st.write('\n')
     st.write('\n')
